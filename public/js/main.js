d9446bcc23c9e9198eae568e3a8e3263480c3ec5
Vaharsolta <vaha-22@mail.ru>
2019-04-24 15:27:01 +0300

    Added function to get item by its id

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
 
 });
 
+/* Functions */
+
+function getId(id){
+    return document.getElementById(id);
+}
+
 function c(str){
     console.log(str);
 }
