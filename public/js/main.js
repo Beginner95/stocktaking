5fdf25e0193c45aab9d34bf044a61678f8f55112
Vaharsolta <vaha-22@mail.ru>
2019-04-24 15:32:08 +0300

    Added function to get items by their class

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,10 @@ function getId(id){
     return document.getElementById(id);
 }
 
+function getCN(cn){
+    return document.getElementsByClassName(cn);
+}
+
 function c(str){
     console.log(str);
 }
