89a7bbf4bf126523f4a86714e6d5718c33d470f8
Vaharsolta <vaha-22@mail.ru>
2019-04-24 18:08:54 +0300

    Added function ajax

--- public/js/main.js
+++ public/js/main.js
@@ -19,3 +19,17 @@ function getQS(qs){
 function c(str){
     console.log(str);
 }
+
+function ajax(method, url, params, callback){
+    let f = callback || function(data){};
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(){
+        if (request.readyState === 4 && request.status === 200) {
+            f(request.responseText);
+        }
+    };
+
+    request.open(method, url);
+    request.send(params);
+}
