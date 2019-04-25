cb41bd115ba4fdac42c6064cc497a09408df2c6c
Vaharsolta <vaha-22@mail.ru>
2019-04-25 11:49:22 +0300

    Mani configuration file

---
+++ private/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'dbname' => 'stocktaking',
+        'user' => 'root',
+        'password' => ''
+    ]
+];
