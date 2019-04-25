9d5546273ab4505c6164b187ac6d865def9e7f83
Vaharsolta <vaha-22@mail.ru>
2019-04-26 00:42:21 +0300

    Added method find by id

--- private/App/Model.php
+++ private/App/Model.php
@@ -21,4 +21,20 @@ abstract class Model
         return $data;
     }
 
+    /**
+     * @param $id
+     * @return bool
+     * @throws DbException
+     */
+    public static function findById($id)
+    {
+        $db = new Db();
+        $data = $db->query(
+            'SELECT * FROM ' . static::$table . 'WHERE id=:id',
+            [':id' => $id],
+            static::class
+        );
+        return $data[0] ?? false;
+    }
+
 }
