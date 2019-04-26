5c6869d8c19f5d09e39127e55d640a1692164da8
Vaharsolta <vaha-22@mail.ru>
2019-04-26 20:16:18 +0300

    Added method getProduct

--- private/App/Controllers/Index.php
+++ private/App/Controllers/Index.php
@@ -17,4 +17,19 @@ class Index
         $this->view->products = $products;
         $this->view->display(__DIR__ . '/../../../views/products.php');
     }
+
+    /**
+     * @return Product|bool
+     * @throws \App\DbException
+     */
+    public function getProduct()
+    {
+        if(isset($_GET['id']) && !empty($_GET['id'])) {
+            $id = $_GET['id'];
+            $product = Product::findById($id);
+        } else {
+            $product = new Product();
+        }
+        return $product;
+    }
 }
