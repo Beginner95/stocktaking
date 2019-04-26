a3b833e39475c08346ba3ed8b1316ee570957c42
Vaharsolta <vaha-22@mail.ru>
2019-04-26 09:12:29 +0300

    Added class for View

---
+++ private/App/View.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+
+class View
+    implements \Countable, \Iterator
+{
+    use TMagic;
+
+    public function display($template)
+    {
+        echo $this->render($template);
+    }
+
+    public function render($template)
+    {
+        ob_start();
+        foreach ($this->data as $var => $value) {
+            $$var = $value;
+        }
+        include $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+
+    public function current()
+    {
+        return current($this->data);
+    }
+
+    public function next()
+    {
+        next($this->data);
+    }
+
+    public function key()
+    {
+        return key($this->data);
+    }
+
+    public function valid()
+    {
+        return null !== key($this->data);
+    }
+
+    public function rewind()
+    {
+        reset($this->data);
+    }
+
+    public function count()
+    {
+        return count($this->data);
+    }
+}
