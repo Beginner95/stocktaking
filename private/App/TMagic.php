7e3c7fa81de791ddb7235b6952ab87d6230d9685
Vaharsolta <vaha-22@mail.ru>
2019-04-26 09:11:18 +0300

    Added class TMagic for View

---
+++ private/App/TMagic.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+
+trait TMagic
+{
+    protected $data = [];
+
+    public function __set($key, $value)
+    {
+        $this->data[$key] = $value;
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->data[$name]);
+    }
+
+    public function __get($key)
+    {
+        return $this->data[$key];
+    }
+}
