55615229bccb787fb94b2e96b808a2ed37f75ace
Vaharsolta <vaha-22@mail.ru>
2019-04-28 11:15:59 +0300

    Added template menu

---
+++ views/menu.php
@@ -0,0 +1,16 @@
+<header>
+    <nav>
+        <ul class="top-menu">
+            <li><a href="#">Файл</a></li>
+            <li><a href="#">Вид</a></li>
+            <li><a href="#">Склад</a>
+                <ul>
+                    <li><a href="#">Категории</a></li>
+                    <li><a href="/index">Товары</a></li>
+                    <li><a href="#">Производители</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Продажи</a></li>
+        </ul>
+    </nav>
+</header>
