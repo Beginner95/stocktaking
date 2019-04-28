eae8ab1f27a4747c294a5bbb72f379f8e6e0f993
Vaharsolta <vaha-22@mail.ru>
2019-04-28 18:15:34 +0300

    Set links for menu

--- views/menu.php
+++ views/menu.php
@@ -5,12 +5,12 @@
             <li><a href="#">Вид</a></li>
             <li><a href="#">Склад</a>
                 <ul>
-                    <li><a href="#">Категории</a></li>
+                    <li><a href="/category">Категории</a></li>
                     <li><a href="/index">Товары</a></li>
-                    <li><a href="#">Производители</a></li>
+                    <li><a href="/manufacturer">Производители</a></li>
                 </ul>
             </li>
-            <li><a href="#">Продажи</a></li>
+            <li><a href="/sales">Продажи</a></li>
         </ul>
     </nav>
 </header>
