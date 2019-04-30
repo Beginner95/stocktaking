57355743c37b935537165b02ff5721dcd5484c93
Vaharsolta <vaha-22@mail.ru>
2019-04-30 12:14:57 +0400

    Added method getUser

--- private/App/Controllers/User.php
+++ private/App/Controllers/User.php
@@ -1,12 +1,30 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controllers;
+
+use App\Controller;
 
 class User
-	extends Model
+	extends Controller
 {
+	/**
+	* @throws \App\DbException
+	*/
 	public function actionDefault()
 	{
-		
+		$users = \App\Model\User::findAll();
+		$this->view->users = $users;
+		$this->view->display(__DIR__ . '/../../../views/users.php');
 	}
-} 
+
+	public function getUser()
+	{
+		if (isset($_GET['id']) && !empty($_GET['id'])) {
+			$id = $_GET['id'];
+			$user = \App\Model\User::findById($id);
+		} else {
+			$user = new \App\Model\User();
+		}
+		return $user;
+	}
+}
