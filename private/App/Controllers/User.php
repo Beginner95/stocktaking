1f58b9df8e3dc57caed5a7f9cbc216c5acc411b6
Vaharsolta <vaha-22@mail.ru>
2019-04-30 12:22:48 +0400

    Added method edit

--- private/App/Controllers/User.php
+++ private/App/Controllers/User.php
@@ -17,6 +17,9 @@ class User
 		$this->view->display(__DIR__ . '/../../../views/users.php');
 	}
 
+	/**
+	* @throws \App\DbException
+	*/
 	public function getUser()
 	{
 		if (isset($_GET['id']) && !empty($_GET['id'])) {
@@ -26,5 +29,35 @@ class User
 			$user = new \App\Model\User();
 		}
 		return $user;
+	}
+
+	/**
+     * @throws \App\DbException
+     */
+	public function actionSave()
+	{
+		$user = $this->getUser();
+		if (!empty($_POST)) {
+			$user->login = strip_tags($_POST['login']);
+			$user->password = strip_tags($_POST['password']);
+			$user->first_name = strip_tags($_POST['first-name']);
+			$user->last_name = strip_tags($_POST['last-name']);
+			$user->second_name = strip_tags($_POST['second_name']);
+			$user->save();
+		}
+	}
+
+	/**
+     * @throws \App\DbException
+     */
+	public function actionEdit()
+	{
+		$this->view->user = $this->getUser();
+
+		if($_GET['ajax'] === 'true') {
+			echo json_encode($this->view->user);
+		} else {
+			$this->view->display(__DIR__ . '/../../../views/users');
+		}
 	}
 }
