e48e8dacfb81bc9034e703e5783139acd3a75a7c
Vaharsolta <vaha-22@mail.ru>
2019-04-30 14:46:11 +0400

    Added function moneyFormat

--- public/js/functions.js
+++ public/js/functions.js
@@ -123,3 +123,7 @@ function getChar(event) {
 
     return null; // специальная клавиша
 }
+
+function moneyFormat(n) {
+    return parseFloat(n).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1 ");
+}
