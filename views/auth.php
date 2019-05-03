08eed0662304afdcc9c8e73291ce4a1b1dd7e02b
Vaharsolta <vaha-22@mail.ru>
2019-05-03 18:01:47 +0300

    Added template for auth

---
+++ views/auth.php
@@ -0,0 +1,27 @@
+<?php include __DIR__ . '/header.php'; ?>
+<div class="container">
+    <main>
+        <div class="modal-form-auth">
+            <div class="modal-form-content">
+                <form id="auth_form">
+                    <fieldset>
+                        <legend class="modal-title">Авторизация</legend>
+                        <label for="login">Логин <span>*</span></label>
+                        <input type="text" name="login" id="login" value=""><br>
+                        <label for="password">Пароль <span>*</span></label>
+                        <input type="text" name="password" id="password" value="">
+                    </fieldset>
+                </form>
+                <a href="#" id="sign_in">Войти</a>
+            </div>
+        </div>
+    </main>
+</div>
+<div id="prompt-form-container">
+    <div id="prompt-form">
+        <div id="prompt-message"></div>
+        <button id="yes">ОК</button>
+    </div>
+</div>
+<?php include __DIR__ . '/footer.php'; ?>
+<script src="/public/js/auth.js"></script>
