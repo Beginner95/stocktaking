ed3b7a568db2463bf4e76473bce36a154ede7776
Vaharsolta <vaha-22@mail.ru>
2019-05-03 18:17:35 +0300

    Added template for denied

---
+++ views/denied.php
@@ -0,0 +1,9 @@
+<?php include __DIR__ . '/header.php'; ?>
+    <div id="prompt-form-container">
+        <div id="prompt-form">
+            <div id="prompt-message"></div>
+            <button id="yes">ОК</button>
+        </div>
+    </div>
+<?php include __DIR__ . '/footer.php'; ?>
+<script src="/public/js/denied.js"></script>
