d24ce02698807009c68bfb91b7bb36ad0ea209b2
Vaharsolta <vaha-22@mail.ru>
2019-05-04 10:43:22 +0300

    Added method actionSearch

--- private/App/Controllers/Index.php
+++ private/App/Controllers/Index.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 
 use App\Controller;
-
+use App\Model\Product;
 
 class Index
     extends Controller
@@ -12,6 +12,17 @@ class Index
     public function actionDefault()
     {
         $this->view->display(__DIR__ . '/../../../views/manager/index.php');
+    }
+
+    /**
+     * @throws \App\DbException
+     */
+    public function actionSearch()
+    {
+        if (!empty($_POST['q'])) {
+            $products = Product::search($_POST['q']);
+            echo json_encode($products);
+        }
     }
 
 }
