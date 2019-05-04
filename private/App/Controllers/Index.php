317ae20b171eaa6f37d6e080aff7298c0ae25502
Vaharsolta <vaha-22@mail.ru>
2019-05-05 02:18:13 +0300

    Added method Checkout for save cart

--- private/App/Controllers/Index.php
+++ private/App/Controllers/Index.php
@@ -2,8 +2,9 @@
 
 namespace App\Controllers;
 
-
 use App\Controller;
+use App\Model\Order;
+use App\Model\OrderProducts;
 use App\Model\Product;
 
 class Index
@@ -35,4 +36,38 @@ class Index
         }
     }
 
+    /**
+     * @throws \App\DbException
+     */
+    public function actionCheckout()
+    {
+        $order = new Order();
+        if (!empty($_POST['products'])) {
+            $total_quantity = (int)$_POST['quantity'];
+            $total_sum = (float)$_POST['total-sum'];
+            $user_id = $_SESSION['user']['id'];
+            $order->quantity = $total_quantity;
+            $order->total_sum = $total_sum;
+            $order->date_added = date('Y-m-d H:i:s');
+            $order->user_id = $user_id;
+            $order->save();
+            $products = json_decode($_POST['products'], true);
+            foreach ($products as $product) {
+                $order_product = new OrderProducts();
+                $prod = Product::findById($product['id']);
+                $order_product->code = $product['code'];
+                $order_product->name = $product['name'];
+                $order_product->price = $product['price'];
+                $order_product->quantity = $product['quantity'];
+                $order_product->total_sum = $product['price'] * $product['quantity'];
+                $order_product->order_id = $order->id;
+                $order_product->save();
+                $prod->quantity = $prod->quantity - $product['quantity'];
+                $prod->save();
+            }
+            echo 1;
+        } else {
+            echo 0;
+        }
+    }
 }
