f14db61e57f252db2d9fc9de81f212e5191c35b0
Vaharsolta <vaha-22@mail.ru>
2019-05-06 13:19:50 +0400

    Added functinality for order

---
+++ public/js/order.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+	let btn_delete = getQSA('.delete');
+
+	for (let i = 0; i < btn_delete.length; i++) {
+		btn_delete[i].onclick = function () {
+			
+		}
+	}
+	
+});
