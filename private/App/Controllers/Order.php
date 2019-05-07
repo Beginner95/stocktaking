8ffe880ff3a11dfa55233adbb41c67a8f2903be8
Vaharsolta <vaha-22@mail.ru>
2019-05-07 13:26:04 +0300

    Deleting an order

--- private/App/Controllers/Order.php
+++ private/App/Controllers/Order.php
@@ -6,9 +6,29 @@ use App\Controller;
 class Order
 	extends Controller
 {
+    /**
+     * @throws \App\DbException
+     */
 	public function actionDefault()
 	{
 		$this->view->orders = \App\Model\Order::findAll();
 		$this->view->display(__DIR__ . '/../../../views/manager/order.php');
 	}
+
+    /**
+     * @throws \App\DbException
+     */
+	public function actionDelete()
+    {
+        if (!empty($_GET['id'])) {
+            $order = \App\Model\Order::findById($_GET['id']);
+
+            if (null !== $order->id) {
+                $order->delete();
+                \App\Model\OrderProducts::deleteOrderProducts($order->id);
+            }
+        }
+    }
+
+
 }
