c47150a0f8ee0581234bc800fb48dc179c2bc8cc
Vaharsolta <vaha-22@mail.ru>
2019-05-07 13:27:33 +0300

    Btn deleting an order

--- public/js/order.js
+++ public/js/order.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	for (let i = 0; i < btn_delete.length; i++) {
 		btn_delete[i].onclick = function () {
-			
+			let order = btn_delete[i].parentNode.parentNode;
+			let order_id = order.getAttribute('id');
+			ajax('GET', '/order/delete/?id=' + order_id, '', function (data) {
+				if (data === '') {
+					order.remove();
+                    showPrompt(order.childNodes[3].innerText + ' был успешно удален!', '', '');
+				}
+            })
 		}
 	}
 	
