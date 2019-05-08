85249b3fe9aad0cba924eebde0d3e7097ce7fb40
Vaharsolta <vaha-22@mail.ru>
2019-05-09 02:46:16 +0300

    Added method getOrderProducts

--- private/App/Model/OrderProducts.php
+++ private/App/Model/OrderProducts.php
@@ -16,6 +16,7 @@ class OrderProducts
     public $quantity;
     public $total_sum;
     public $order_id;
+    public $product_id;
 
     /**
      * @param $order_id
@@ -27,6 +28,22 @@ class OrderProducts
         $sql = 'DELETE FROM ' . static::$table . ' WHERE order_id=:order_id';
         $db = new Db();
         $db->execute($sql, $data);
+    }
+
+    /**
+     * @param $order_id
+     * @return bool
+     * @throws \App\DbException
+     */
+    public static function getOrderProducts($order_id)
+    {
+        $db = new Db();
+        $data = $db->query(
+            'SELECT * FROM ' . static::$table . ' WHERE order_id=:order_id',
+            [':order_id' => $order_id],
+            static::class
+        );
+        return $data ?? false;
     }
 
 }
