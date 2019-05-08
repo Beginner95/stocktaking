6c2c379534faa9f437950dedcd2adc41a67d6d77
Vaharsolta <vaha-22@mail.ru>
2019-05-09 02:53:12 +0300

    Added cancellation feature

--- public/js/order.js
+++ public/js/order.js
@@ -1,17 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
 	let btn_delete = getQSA('.delete');
+	let btn_return = getQSA('.return');
 
 	for (let i = 0; i < btn_delete.length; i++) {
+        let order = btn_delete[i].parentNode.parentNode;
+        let order_id = order.getAttribute('id');
 		btn_delete[i].onclick = function () {
-			let order = btn_delete[i].parentNode.parentNode;
-			let order_id = order.getAttribute('id');
 			ajax('GET', '/order/delete/?id=' + order_id, '', function (data) {
 				if (data === '') {
 					order.remove();
                     showPrompt(order.childNodes[3].innerText + ' был успешно удален!', '', '');
 				}
             })
-		}
+		};
+
+		btn_return[i].onclick = function () {
+			ajax('GET', '/order/return/?id=' + order_id, '', function (data) {
+				if (data === '') {
+                    order.remove();
+                    showPrompt(order.childNodes[3].innerText + ' был успешно отменен!', '', '');
+				}
+            })
+        };
 	}
-	
 });
