b8e3f6c80aa844be9a5af037b39ecb08460cbe82
Vaharsolta <vaha-22@mail.ru>
2019-05-09 11:42:22 +0300

    Autocomplete off for all inputs

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,4 +1,11 @@
 /* Functions */
+document.addEventListener('DOMContentLoaded', function () {
+    let inputs = document.getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].setAttribute('autocomplete', 'off');
+    }
+});
+
 var showingTooltip;
 function getId(id){
     return document.getElementById(id);
