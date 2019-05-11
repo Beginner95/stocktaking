d4aea4efaa814914db9324e1dfac09831e08aa2c
Vaharsolta <vaha-22@mail.ru>
2019-05-11 12:07:04 +0300

    Added functionality printing

--- public/js/order.js
+++ public/js/order.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	let btn_delete = getQSA('.delete');
 	let btn_return = getQSA('.return');
 	let btn_show = getQSA('.show');
+	let btn_print = getQSA('.print');
 	let modal_form_order_products = getQS('.modal-form-order-products');
 	let table_products = getQS('.table-cart');
     let close_modal_form = getQS('.btn-close');
@@ -47,6 +48,30 @@ document.addEventListener('DOMContentLoaded', function () {
                 table_products.childNodes[3].innerHTML = arr.join('');
                 table_products.childNodes[5].innerHTML = '<tr><td colspan="5">Итого:</td><td>' + order.childNodes[9].innerHTML + '</td></tr>';
             })
-        }
+        };
+
+        btn_print[i].onclick = function () {
+            let table = cE('table');
+            table.id = 'print';
+            table.innerHTML += '<thead><tr><th colspan="6"><h2>Заказ №' + order_id + ' от ' + order.childNodes[11].innerHTML + '</h2></th></tr><tr><th>№</th><th>Артикул</th><th>Наименование</th><th>Цена</th><th>Количество</th><th>Сумма</th></tr></thead>';
+            ajax('GET', '/order/products/?order_id=' + order_id, '', function (data) {
+                data = JSON.parse(data);
+                let arr = [];
+                let num = 1;
+                for (let i = 0; i < data.length; i++) {
+                    arr[i] = '<tr id="' + data[i].id + '"><td>' + num++ + '</td><td>' + data[i].code + '</td><td>' + data[i].name + '</td><td>' + moneyFormat(data[i].price) + '</td><td>' + data[i].quantity + '</td><td>' + moneyFormat(data[i].price * data[i].quantity) + '</td></tr>';
+
+                }
+                table.innerHTML += arr.join('');
+                table.innerHTML += '<tfoot><tr><td colspan="5">Итого:</td><td>' + order.childNodes[9].innerHTML + '</td></tr></tfoot>';
+
+                getQS('body').insertBefore(table, null);
+
+                PrintElem(table);
+
+            });
+            return false;
+        };
+
 	}
 });
